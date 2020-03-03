1fc4bd036fb93965214d2ad3d9714c45d6c4ccdb
Sreedhar <git.suswaram@gmail.com>
2020-03-03 13:04:01 -0700

    Added Header Nav with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Header from './components/layout/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
 class App extends Component {
 
   render() {
